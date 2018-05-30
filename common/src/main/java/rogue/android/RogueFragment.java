598abeddac537182dcd7159c6fc7d97ff742b540
rogue <silencewalker94@gmail.com>
2018-05-30 10:39:41 +0700

    added RogueFragment

---
+++ common/src/main/java/rogue/android/RogueFragment.java
@@ -0,0 +1,35 @@
+package rogue.android;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public abstract class RogueFragment extends Fragment {
+    private Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutRes(), container);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @LayoutRes
+    public abstract int getLayoutRes();
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
+
